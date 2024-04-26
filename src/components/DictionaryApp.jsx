b2a805557d12a0286769824f671755a0d0cea0f4
aliwert <154356044+aliwert@users.noreply.github.com>
2024-04-26 15:13:57 +0300

    icons added

--- src/components/DictionaryApp.jsx
+++ src/components/DictionaryApp.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Switch from "./Switch";
 
 function DictionaryApp() {
   const [word, setWord] = useState("");
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  darkMode: "selector",
+  content: ["./src/**/*.{html,js,jsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
+  
 };
